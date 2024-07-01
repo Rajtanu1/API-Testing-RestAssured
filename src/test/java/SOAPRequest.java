f7b00ed9ad5d486b397a59b8b4a6c8f5f9973c3f
Rajtanu Chakravarty <rajtanuchakravarty@gmail.com>
2024-07-01 22:55:55 +0530

    Added a java file with class name "SOAPRequest"

---
+++ src/test/java/SOAPRequest.java
@@ -0,0 +1,31 @@
+import static io.restassured.RestAssured.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+public class SOAPRequest {
+	@Test
+     public void postRequest() throws IOException {
+          baseURI = "https://ecs.syr.edu";
+          
+          File file = new File("./src/test/resources/AddNumbers.xml"); // Created a file object referring to AddNumbers.xml file 
+          FileInputStream fileData = new FileInputStream(file); // Storing data of xml file to the fileInput variable
+          
+          String payload = IOUtils.toString(fileData, "UTF-8"); // Converting xml file data to a string 
+          
+          given().
+               contentType("text/xml").
+               accept(ContentType.XML).
+               body(payload).
+          when().
+               post("/faculty/fawcett/Handouts/cse775/code/calcWebService/Calc.asmx").
+          then().
+               statusCode(200);   	 
+     }
+}
