eb6510ace147f72854adbe336199cd70475bf5fe
Rajtanu Chakravarty <rajtanuchakravarty@gmail.com>
2024-07-03 23:02:10 +0530

    Imported Hamcrest Matchers to perform assertions on responses

--- src/test/java/SOAPRequest.java
+++ src/test/java/SOAPRequest.java
@@ -9,13 +9,15 @@ import org.apache.commons.io.IOUtils;
 import org.testng.annotations.Test;
 
 import io.restassured.http.ContentType;
+import static org.hamcrest.Matchers.equalTo;;
+
 public class SOAPRequest {
 	@Test
      public void postRequest() throws IOException {
           baseURI = "https://ecs.syr.edu";
           
           File file = new File("./src/test/resources/AddNumbers.xml"); // Created a file object referring to AddNumbers.xml file 
-          FileInputStream fileData = new FileInputStream(file); // Storing data of xml file to the fileInput variable
+          FileInputStream fileData = new FileInputStream(file); // Storing data of xml file to the fileData variable
           
           String payload = IOUtils.toString(fileData, "UTF-8"); // Converting xml file data to a string 
           
@@ -26,6 +28,7 @@ public class SOAPRequest {
           when().
                post("/faculty/fawcett/Handouts/cse775/code/calcWebService/Calc.asmx").
           then().
-               statusCode(200);   	 
+               statusCode(200).
+               body("//AddResult.text()", equalTo("45")); // Validating AddResult element value in the xml body with equalto assertion method  	 
      }
 }
